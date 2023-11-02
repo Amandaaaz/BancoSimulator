44969ff61badb4c5a523fd25569d2a953f87ab91
Amanda Fernandes <95643803+Amandaaaz@users.noreply.github.com>
2023-11-02 13:18:05 -0300

    all

---
+++ script.js
@@ -0,0 +1,57 @@
+    const banco = {
+        conta: "12345",
+        agencia: "7080",
+        tipoConta: "Corrente",
+        saldo: 1000,
+    
+        buscarSaldo: function () {
+        return this.saldo;
+        },
+    
+        deposito: function (valor) {
+        this.saldo += valor;
+        return this.saldo;
+        },
+    
+        saque: function (valor) {
+        if (valor <= this.saldo) {
+            this.saldo -= valor;
+            return this.saldo;
+        } else {
+            return "Saldo insuficiente.";
+        }
+        },
+    
+        numeroConta: function () {
+        return this.conta;
+        },
+    };
+
+    function mostrarSaldo() {
+        const saldoContainer = document.getElementById("saldoContainer");
+        const saldoElement = document.getElementById("saldo");
+    
+        saldoElement.textContent = banco.buscarSaldo();
+        saldoContainer.style.display = "block";
+    }
+    
+    function realizarDeposito() {
+        const valor = parseFloat(prompt("Digite o valor do depósito:"));
+        if (!isNaN(valor)) {
+        banco.deposito(valor);
+        mostrarSaldo();
+        }
+    }
+    
+    function realizarSaque() {
+        const valor = parseFloat(prompt("Digite o valor do saque:"));
+        if (!isNaN(valor)) {
+        const resultado = banco.saque(valor);
+        if (typeof resultado === "number") {
+            mostrarSaldo();
+        } else {
+            alert(resultado);
+        }
+        }
+    }
+    
